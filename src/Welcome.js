20a933fb5a012330aa2d33f4302512bcf713590b
SeleneVicenza <selene.vicenza@hotmail.it>
2022-10-08 14:32:44 +0200

    props-02

--- src/Welcome.js
+++ src/Welcome.js
@@ -1,10 +1,19 @@
 import React from "react";
 
 export class Welcome extends React.Component {
+
+    static defaultProps = {
+        name: 'guest'
+    }
     render() {
         return <p>Welcome, {this.props.name}</p>;
     }
 }
+
+
+// Welcome.defaultProps = {
+//     name: 'guest'
+// }
 
 // export function Welcome(props) {
 //     return <p>Welcome, {props.name}</p>;
