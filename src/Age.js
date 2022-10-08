5da21555a1e2563c4fdde0bcb3a35d744c4d274b
SeleneVicenza <selene.vicenza@hotmail.it>
2022-10-08 17:37:39 +0200

    c-r-5

--- src/Age.js
+++ src/Age.js
@@ -2,6 +2,8 @@ import React from "react";
 
 export default class Age extends React.Component{
     render() {
-        return this.props.age > 18 && this.props.age < 65 && this.props.name === 'John' && <p>Your age is: {this.props.age}</p>
+        return this.props.age > 18 
+        ? <p>Your age is: {this.props.age}</p>
+        : <p>You are very young</p>
     }
 }
