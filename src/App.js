638e38c97adeb81aa5635ed43fd1c0dc3335013b
SeleneVicenza <selene.vicenza@hotmail.it>
2022-10-08 10:48:21 +0200

    props 01

--- src/App.js
+++ src/App.js
@@ -1,9 +1,25 @@
 import React from "react";
 import Hello from "./Hello";
+import {Welcome} from "./Welcome";
 
-export default class App extends React.Component {
-    render(){
-        return <Hello/>;
-    }   
-    
+// export default class App extends React.Component {
+//     render(){
+//         return(
+//             <div>
+//                <Hello />
+//                <Welcome name="Selene" />
+
+//             </div> 
+//         );
+//     }   
+
+// }
+
+export default function App() {
+    return (
+        <div>
+            <Hello />
+            <Welcome name="Selene" />
+        </div>
+    );
 }
--- src/index.js
+++ src/index.js
@@ -3,5 +3,12 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 
 
-const root = ReactDOM.createRoot(document.querySelector('#root'));
-root.render(<App />);
+// const root = ReactDOM.createRoot(document.querySelector('#root'));
+// root.render(<App />);
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
