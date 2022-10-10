0da84694fcc33c8bfa23b239fe9be9d88df29db4
SeleneVicenza <selene.vicenza@hotmail.it>
2022-10-10 14:19:34 +0200

    state-04

--- src/Counter.js
+++ src/Counter.js
@@ -2,11 +2,38 @@ import React from "react";
 import CounterDisplay from "./CounterDisplay";
 
 export default class Counter extends React.Component{
-  render() {
-    return <CounterDisplay/>
-  }
+    state = {
+        count: this.props.initial
+    }
+
+    constructor(props) {
+        super(props)
+
+       
+
+        // setInterval(() => {
+        //     return this.state.count = state.count + 1;
+        // }, 1000);
+
+        setInterval(() => {
+            this.setState((state) => {
+                return {
+                    count: state.count + this.props.increment
+                }
+            });
+        }, this.props.interval);
+    }
+    
+
+    render() {
+        return <CounterDisplay count={this.state.count}/>
+    }
 }
 
-
+Counter.defaultProps = {
+    initial : 0,
+    increment : 1,
+    interval : 1000
+}
 
 
--- src/CounterDisplay.js
+++ src/CounterDisplay.js
@@ -1,36 +1,7 @@
 import React from "react";
 
 export default class CounterDisplay extends React.Component{
-    state = {
-        count: this.props.initial
-    }
-
-    constructor(props) {
-        super(props)
-
-       
-
-        // setInterval(() => {
-        //     return this.state.count = state.count + 1;
-        // }, 1000);
-
-        setInterval(() => {
-            this.setState((state) => {
-                return {
-                    count: state.count + this.props.increment
-                }
-            });
-        }, this.props.interval);
-    }
-    
-
     render() {
-        return <h1>count: {this.state.count}</h1>
+        return <h1>count: {this.props.count}</h1>
     }
-}
-
-CounterDisplay.defaultProps = {
-    initial : 0,
-    increment : 1,
-    interval : 1000,
 }
