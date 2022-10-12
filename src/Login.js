201c6a5c3e756b2e16ae5104eb7a6f5282207759
SeleneVicenza <selene.vicenza@hotmail.it>
2022-10-12 20:22:22 +0200

    form 02

---
+++ src/Login.js
@@ -0,0 +1,34 @@
+import React from "react";
+
+export default class Login extends React.Component {
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            user: '',
+            pass: '',
+            remember: true,
+        }
+    }
+
+    handleInput = (ev) =>{
+        const name = ev.target.name
+        const value = ev.target.type === 'checkbox' ? ev.target.checked : ev.target.value
+
+        this.setState(() => {
+           return {[name] : value}
+        })
+    }
+
+
+    render() {
+        return (
+                <form>
+                    <input type="text" name='user' value={this.state.user} onChange={this.handleInput}></input>
+                    <input type="password" name='pass'  value={this.state.pass} onChange={this.handleInput}></input>
+                    <input type="checkbox" name='remember' checked={this.state.remember} onChange={this.handleInput}></input>
+                    
+                </form>
+        )
+    }
+}
