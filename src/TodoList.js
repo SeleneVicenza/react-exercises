544037fdebfbc788cba02a3724b6d9dc371a3168
SeleneVicenza <selene.vicenza@hotmail.it>
2022-10-16 21:26:14 +0200

    lists-05

--- src/TodoList.js
+++ src/TodoList.js
@@ -5,30 +5,39 @@ export default class TodoList extends React.Component {
         items: [],
         input: ''
     }
-    
+
+
+    clearArray = () => {
+        this.setState({items: []})
+    }
 
     saveInput = (e) => {
-        this.setState({input: e.target.value})
+        this.setState({ input: e.target.value })
     }
 
     addTask = () => {
-        this.setState({items: [...this.state.items, this.state.input]})
+        this.setState({ items: [...this.state.items, this.state.input] })
 
-        this.setState({input: ''})
+        this.setState({ input: '' })
     }
 
     render() {
-        return(
+        return (
             <div>
 
                 <ul>
                     <input type='text' onChange={this.saveInput} value={this.state.input}></input>
                     <button onClick={this.addTask}>Add task</button>
+                    <button onClick={this.clearArray}>Reset</button>
                     {this.state.items.map((item, index) => {
-                        return <li key={index}>{item}</li>
+                        return (<div>
+                            <li key={index}>{item}</li>
+                         
+                        </div>)
+
                     })}
                 </ul>
-            </div> 
+            </div>
         )
     }
 }
