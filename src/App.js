6777ca7153cf38745a5ed42575bdd04b9f8ae285
SeleneVicenza <selene.vicenza@hotmail.it>
2022-10-16 23:29:11 +0200

    component-composition-01

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import ClickCounter from "./ClickCounter";
 import ClickTracker from "./ClickTracker";
+import Container from "./Container";
 import Counter from "./Counter";
 import Hello from "./Hello";
 import InteractiveWelcome from "./InteractiveWelcome";
@@ -13,7 +14,7 @@ import {Welcome} from "./Welcome";
 export class App extends React.Component {
     render(){
         return(
-            <div>
+            <Container>
                <Hello />
                <Welcome name={<strong>Selene</strong>} />
                <Counter />
@@ -23,7 +24,7 @@ export class App extends React.Component {
                <Login />
                <UncontrolledLogin />
                <TodoList />
-            </div> 
+            </Container> 
         );
     }   
 }
