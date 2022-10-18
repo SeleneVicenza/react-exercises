29bdb592c2577de106b8a05a26b659d676a75edf
SeleneVicenza <selene.vicenza@hotmail.it>
2022-10-18 10:38:48 +0200

    context-02

--- src/DisplayLanguage.js
+++ src/DisplayLanguage.js
@@ -1,7 +1,10 @@
 import React from "react";
+import { LanguageContext } from "./Container";
 
-
-
+const Strings = {
+    en: {LANG: "english"},
+    it: {LANG: "italiano"}
+}
 
 export default class DisplayLanguage extends React.Component {
 
@@ -10,7 +13,13 @@ export default class DisplayLanguage extends React.Component {
     render() {
         return (
             <div>
-                
+                <LanguageContext.Consumer>
+                    {(language)=>{
+                        return (<h1>
+                            selected language is: {Strings[language].LANG}
+                        </h1>)
+                    }}
+                </LanguageContext.Consumer>
             </div>
         )
     }
