4404c4d5cd0c6711ad0511ca14d94fe29e4f5d7e
SeleneVicenza <selene.vicenza@hotmail.it>
2022-10-19 18:18:44 +0200

    useState-01

--- src/ClickCounter.js
+++ src/ClickCounter.js
@@ -1,25 +1,42 @@
-import React from "react"
+import React, { useState } from "react"
 
-export default class ClickCounter extends React.Component {
-    state = {
-        count: 0,
+// export default class ClickCounter extends React.Component {
+//     state = {
+//         count: 0,
 
+//     }
+
+
+
+//     clickCounter = () => {
+//         this.setState((state) => {
+//             return {count: state.count + 1}
+//         });
+//     }
+
+//     render() {
+//         return (
+//             <div>
+//                 <button onClick={this.clickCounter}>Click counter</button>
+//                 <h2>count: {this.state.count}</h2>
+//             </div>
+//         )
+//     }
+// }
+
+
+export default function ClickCounter({initialValue = 0}) {
+    const [count, setCount] = useState(initialValue)
+
+    function clickCounter() {
+        setCount(count => count + 1)
     }
+    
 
-
-
-    clickCounter = () => {
-        this.setState((state) => {
-            return {count: state.count + 1}
-        });
-    }
-
-    render() {
-        return (
+    return (
             <div>
-                <button onClick={this.clickCounter}>Click counter</button>
-                <h2>count: {this.state.count}</h2>
+                <button onClick={clickCounter}>Click counter</button>
+                <h2>count: {count}</h2>
             </div>
-        )
-    }
+    )
 }
